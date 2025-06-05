6e266dad69104fd253a0e4542c82dac6b0040cf5
Samuel Alejandro <brmou319367@ab-inbev.com>
2025-06-04 23:53:30 -0300

    feat(translations-table): enhance translation management UI - add icons for create and delete operations with corresponding actions - update styles to reflect operation types with color coding - introduce revert and remove functionalities for translations

--- web/src/app/pages/translations/table/translations-table.component.ts
+++ web/src/app/pages/translations/table/translations-table.component.ts
@@ -2,17 +2,20 @@ import { Component, input, output } from "@angular/core";
 import { Translation } from "../../../shared/models/translation";
 import { TranslationLanguage } from "../../../shared/models/translation-language";
 import { TranslationsTableItemComponent } from "./item/translations-table-item.component";
+import { IconComponent } from "../../../shared/components/icon/icon.component";
 
 
 @Component({
     selector: "app-translations-table",
     templateUrl: "./translations-table.component.html",
     styleUrl: "./translations-table.component.scss",
-    imports: [TranslationsTableItemComponent]
+    imports: [TranslationsTableItemComponent, IconComponent]
 })
 export class TranslationsTableComponent {
     languages = input.required<TranslationLanguage[]>();
     translations = input.required<Translation[]>()
     edit = output<{ translation: Translation, language: TranslationLanguage }>();
     reset = output<{ path: string, language: string }>();
+    remove = output<string>();
+    revertTranslation = output<string>();
 }
