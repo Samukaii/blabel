6a60ee5e77c2a60e6c8c5e5483aa41086fe76c71
Samuel Alejandro <brmou319367@ab-inbev.com>
2025-06-04 23:52:26 -0300

    feat(translations-route): enhance translation change management routes - rename and restructure routes for registering and saving changes - add new routes for reverting and discarding translation changes

--- api/src/routes/translations-route.ts
+++ api/src/routes/translations-route.ts
@@ -4,8 +4,12 @@ import { translationsController } from '../controllers/translations/translations
 const router = express.Router();
 
 router.get('/', translationsController.getAll);
-router.post('/register_change', translationsController.registerChange);
-router.post('/save_all', translationsController.saveAll);
-router.patch('/reset_change', translationsController.resetChange);
+router.post('/changes', translationsController.registerChange);
+router.post('/:path/changes/deletion', translationsController.registerRemoveChange);
+router.post('/changes/save', translationsController.saveAll);
+
+router.delete('/:path/changes/revert', translationsController.revertTranslationChange);
+router.delete('/:path/changes/entries/:language/revert', translationsController.revertEntryChange);
+router.delete('/changes/discard_all', translationsController.discardAllChanges);
 
 export const translationsRoute = router;
