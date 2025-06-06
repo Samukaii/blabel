d433a3dab6db42f38a9e0c6763e8df6b35afe353
Samuel Alejandro <brmou319367@ab-inbev.com>
2025-06-06 01:00:58 -0300

    feat(openai): add new service for structured output parsing
    - create openai.service.ts with structuredOutput function
    - utilize zod for schema validation
    - integrate openAiClient for parsing responses

---
+++ api/src/services/openai/openai.service.ts
@@ -0,0 +1,20 @@
+import { z, ZodObject, ZodSchema } from 'zod';
+import { zodTextFormat } from "openai/helpers/zod";
+import { openAiClient } from '../../core/open-ai-client';
+import { AiMessage } from '../../models/ai-message';
+
+const structuredOutput = async <T extends Record<string, any>>(messages: AiMessage[], schema: ZodObject<T>) => {
+    const response = await openAiClient().responses.parse({
+        model: 'gpt-4o-mini',
+        input: messages,
+        text: {
+            format: zodTextFormat(schema, 'result')
+        }
+    });
+
+    return response.output_parsed as T;
+}
+
+export const openAiService = {
+    structuredOutput
+};
