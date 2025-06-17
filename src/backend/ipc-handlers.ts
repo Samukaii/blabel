14c8ff807623974afd91976c9898d77e7f016919
Samuel Alejandro <brmou319367@ab-inbev.com>
2025-06-17 15:04:16 -0300

    refactor(ipc-handlers): correct import of developmentHandler in IPC handlers

--- src/backend/ipc-handlers.ts
+++ src/backend/ipc-handlers.ts
@@ -1,7 +1,7 @@
 import { ElectronFeatures } from '@shared/models/electron-features.js';
 import { aiHintsHandler } from './handlers/ai-hints/ai-hints.handler';
 import { applicationLanguagesHandler } from './handlers/application-languages/application-languages.handler';
-import { developmentHandlers } from './handlers/development/development-handlers.js';
+import { developmentHandler } from './handlers/development/development-handler.js';
 import { filesHandler } from './handlers/files/files.handler.js';
 import { translationsHandler } from './handlers/translations/translations.handler.js';
 import { windowHandler } from './handlers/window/window.handler.js';
@@ -14,5 +14,5 @@ export const ipcHandlers: ElectronFeatures = {
 		languages: applicationLanguagesHandler,
 		aiHints: aiHintsHandler
 	},
-	development: developmentHandlers
+	development: developmentHandler
 };
