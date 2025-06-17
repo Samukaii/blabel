fdfae466c585b48d35671040f386e474ad7d0740
Samuel Alejandro <brmou319367@ab-inbev.com>
2025-06-17 14:38:35 -0300

    refactor(local-json-resource): update path handling to use electron's userData directory for improved file management

--- src/backend/core/local-json-resource.ts
+++ src/backend/core/local-json-resource.ts
@@ -1,7 +1,12 @@
-import { fileManager } from "./file-manager.js";
+import { fileManager } from './file-manager.js';
+import * as electron from 'electron';
 
-export const localJsonResource = <T extends (Record<string, any> | Record<string, any>[])>(path: string) => {
-	const manager = fileManager(path);
+export const localJsonResource = <
+	T extends Record<string, any> | Record<string, any>[],
+>(
+	path: string,
+) => {
+	const manager = fileManager(`${electron.app.getPath('userData')}/${path}`);
 	let lastResult: T | null = null;
 
 	const get = async () => {
@@ -21,11 +26,11 @@ export const localJsonResource = <T extends (Record<string, any> | Record<string
 		if (!!lastResult) return true;
 
 		return await manager.exists();
-	}
+	};
 
 	const update = async (fn: (content: T) => T) => {
 		await save(fn(await get()));
-	}
+	};
 
-	return {get, save, update, exists};
+	return { get, save, update, exists };
 };
