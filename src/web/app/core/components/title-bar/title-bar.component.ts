e1bb5fef2c2b0aaf36ffee599eb43f74c6a56dfb
Samuel Alejandro <brmou319367@ab-inbev.com>
2025-06-17 15:05:06 -0300

    refactor(title-bar): enhance debug tool access and improve code structure

--- src/web/app/core/components/title-bar/title-bar.component.ts
+++ src/web/app/core/components/title-bar/title-bar.component.ts
@@ -1,28 +1,32 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { getElectron } from 'web/app/shared/di/functions/get-electron';
 import { IconComponent } from "../../../shared/components/icon/icon.component";
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-title-bar',
-  imports: [IconComponent],
+  imports: [IconComponent, AsyncPipe],
   templateUrl: './title-bar.component.html',
   styleUrl: './title-bar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TitleBarComponent {
 	private electron = getElectron();
+	protected isDebugAllowed = this.electron.development.isDebugAllowed();
 
-	openDevTools() {
+	protected openDevTools() {
 		this.electron.development.openDevTools();
 	}
 
-	close() {
+	protected close() {
 		this.electron.window.close();
 	}
-	maximize() {
+
+	protected maximize() {
 		this.electron.window.maximize();
 	}
-	minimize() {
+
+	protected minimize() {
 		this.electron.window.minimize();
 	}
 }
