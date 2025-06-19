ce5d852321a95f2b55c7af809ea1ccfefe3bb751
samukaii <samuelalejandro.dev@gmail.com>
2025-06-19 03:47:17 -0300

    feat(routes): add dynamic redirect based on configuration
    - implement async redirectTo function for default route
    - check electron configuration to determine initial route
    - redirect to "translations" if configuration exists, else to "languages"

--- src/web/app/app.routes.ts
+++ src/web/app/app.routes.ts
@@ -1,12 +1,21 @@
 import { Routes } from '@angular/router';
 import { TranslationsComponent } from './pages/translations/translations.component';
 import { LanguagesComponent } from './pages/languages/languages.component';
+import { getElectron } from './shared/di/functions/get-electron';
 
 export const routes: Routes = [
     {
         path: "",
         pathMatch: "full",
-        redirectTo: "translations"
+		redirectTo: async () => {
+			const electronFeatures = getElectron();
+
+			const existsConfiguration = await electronFeatures.api.languages.get();
+
+			if(existsConfiguration.results.length) return "translations";
+
+			return  'languages';
+		}
     },
     {
         path: "translations",
