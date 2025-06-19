aea38a1a5bd05840ddbab489c582b79c4a8653c9
samukaii <samuelalejandro.dev@gmail.com>
2025-06-19 01:49:49 -0300

    feat(utils): enhance outsideClickEffect with options
    - introduce OutsideClickEffectOptions interface for configuration
    - add an excludeIds option to ignore specific elements during clicks

--- src/web/app/shared/utils/outside-click-effect.ts
+++ src/web/app/shared/utils/outside-click-effect.ts
@@ -1,12 +1,24 @@
 import { DestroyRef, DOCUMENT, ElementRef, inject } from "@angular/core";
 
-export const outsideClickEffect = (fn: () => void) => {
+interface OutsideClickEffectOptions {
+	excludeIds?: string[];
+}
+
+export const outsideClickEffect = (fn: () => void, options?: OutsideClickEffectOptions) => {
 	const elementRef = inject(ElementRef);
 	const destroyRef = inject(DestroyRef);
 	const document = inject(DOCUMENT);
 
 	const handleClickOutside = (event: MouseEvent) => {
 		const wrapper = elementRef.nativeElement;
+
+		const excludedElements = options?.excludeIds
+			?.map(id => document.getElementById(id))
+			?.filter(element => !!element) ?? [];
+
+		const isAnExcludedElement = excludedElements.some(excluded => excluded.contains(event.target as HTMLElement));
+
+		if(isAnExcludedElement) return;
 
 		if (!wrapper.contains(event.target)) fn();
 	};
